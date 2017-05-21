7ba06b764dbaca46f16f91ef0af1ff947ddc8d60
Michał Ligęza <mligeziak@gmail.com>
2017-05-21 12:59:56 +0200

    fix headers

--- src/Controller/AppusersController.php
+++ src/Controller/AppusersController.php
@@ -24,7 +24,8 @@ class AppusersController extends AppController
     {
         if (in_array($this->request->action, ['signup', 'login'])) {
             $this->response->header('Access-Control-Allow-Origin', '*');
-            $this->response->header('Access-Control-Allow-Headers', '*');
+            $this->response->header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+            $this->response->header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
         }
     }
 
